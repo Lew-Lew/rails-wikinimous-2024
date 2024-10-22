4a13d1a9d67d31aa4c8838356511c71fbcda0fd1
Louise Ouldhaddi <ouldhaddi@gmail.com>
2024-10-22 17:55:55 +0200

    controller & form new

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  resources :articles
+  root to: "articles#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning Articles"
+Article.destroy_all
+
+10.times do
+  Article.create(title: Faker::Book.title, content: Faker::Quote.yoda)
+end
+
+puts "10 articles created"
